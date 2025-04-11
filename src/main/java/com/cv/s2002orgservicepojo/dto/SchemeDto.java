4db844cfb7484b19d24ea81e2b538f0508b0b551
dpak8 <dpak804@gmail.com>
2025-04-11 17:15:48 +0530

    Commit on 11042025

---
+++ src/main/java/com/cv/s2002orgservicepojo/dto/SchemeDto.java
@@ -0,0 +1,26 @@
+package com.cv.s2002orgservicepojo.dto;
+
+import com.cv.s10coreservice.dto.generic.GenericDto;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serializable;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@AllArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class SchemeDto extends GenericDto implements Serializable {
+
+    @NotBlank(message = "{app.message.failure.blank}")
+    @NotNull(message = "{app.message.failure.blank}")
+    @Size(min = 3, max = 250, message = "{app.message.failure.size}")
+    private String schemeCode;
+
+}
