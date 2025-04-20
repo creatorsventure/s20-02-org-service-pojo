395221aae20932fa81331adf89cfcf5447006bfa
ram <ram_r@live.com>
2025-04-20 16:41:46 +0400

    Commit 20-04-2025 - ORG Portal features

---
+++ src/main/java/com/cv/s2002orgservicepojo/dto/EngineDto.java
@@ -0,0 +1,37 @@
+package com.cv.s2002orgservicepojo.dto;
+
+import com.cv.s10coreservice.dto.generic.GenericDto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@SuperBuilder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString(callSuper = true)
+@EqualsAndHashCode(callSuper = true)
+public class EngineDto extends GenericDto implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = -8413128872046476638L;
+
+    @NotBlank(message = "{app.message.failure.blank}")
+    @NotNull(message = "{app.message.failure.blank}")
+    @Size(min = 3, max = 25, message = "{app.message.failure.size}")
+    private String code;
+
+    @NotBlank(message = "{app.message.failure.blank}")
+    @NotNull(message = "{app.message.failure.blank}")
+    @Size(min = 3, max = 250, message = "{app.message.failure.size}")
+    private String address;
+
+    @NotNull(message = "{app.message.failure.blank}")
+    private Integer priority;
+
+}
