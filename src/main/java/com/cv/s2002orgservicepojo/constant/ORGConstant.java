1b0ef9ec6b3a364f61a6705bd158276b22a53676
ram <ram_r@live.com>
2025-05-08 22:31:35 +0400

    COMMIT 08-05-2025

--- src/main/java/com/cv/s2002orgservicepojo/constant/ORGConstant.java
+++ src/main/java/com/cv/s2002orgservicepojo/constant/ORGConstant.java
@@ -7,6 +7,7 @@ public class ORGConstant {
     public static final String APP_NAVIGATION_API_UNIT = "/unit";
     public static final String APP_NAVIGATION_API_RESOLVE_UNIT_ID = "/resolve-id";
     public static final String APP_NAVIGATION_API_RESOLVE_UNIT_ID_NAME_MAPS = "/resolve-unit-id-name-maps";
+    public static final String APP_NAVIGATION_API_RESOLVE_UNIT_SCHEME = "/resolve-unit-scheme";
     public static final String APP_NAVIGATION_API_UNIT_SIGNUP = "/sign-up";
     public static final String APP_NAVIGATION_API_ACTION = "/action";
     public static final String APP_NAVIGATION_API_CURRENCY = "/currency";
